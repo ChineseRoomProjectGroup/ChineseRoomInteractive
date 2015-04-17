d5fe1807fcfe6b3006ec3cfb87158592fd3ed7da
Robert Wild <wildr2@rpi.edu>
2015-04-17 17:02:53 -0400

    Initial Commit

---
+++ ChineseRoomInteractiveUnity/Assets/Scripts/HandController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandController : MonoBehaviour 
+{
+
+    public Transform tip_position;
+
+	public void Update()
+    {
+        Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mouse_pos - (Vector2)tip_position.localPosition;
+
+    }
+}
