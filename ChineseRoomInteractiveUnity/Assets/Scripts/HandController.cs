9eb32becc84286ea4da08aa3ad46d6ce1b413f68
yesmuffin <yes@rpi.edu>
2015-04-18 16:49:01 -0400

    Hand Controller Modified
    On click

--- ChineseRoomInteractiveUnity/Assets/Scripts/HandController.cs
+++ ChineseRoomInteractiveUnity/Assets/Scripts/HandController.cs
@@ -8,8 +8,20 @@ public class HandController : MonoBehaviour
 
 	public void Update()
     {
+        //-----hand following mouse-------------------------------------
         Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mouse_pos - (Vector2)tip_position.localPosition;
 
+        //-----on left button click, hand change to down position-------
+        if (Input.GetMouseButtonDown(0))
+        {
+            // change the sprite to hand_down
+        }
+        else
+        {
+            // change the sprite to hand_up
+        }
+
+        // 
     }
 }
