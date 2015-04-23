cdc688d6941415de87acd80917572341ac455665
Robert Wild <wildr2@rpi.edu>
2015-04-23 18:46:48 -0400

    Mecanim Animation Prototype

---
+++ ChineseRoomInteractiveUnity/Assets/Scripts/ProgressionAnim.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressionAnim : MonoBehaviour 
+{
+    public Animator animator;
+
+    public void Next()
+    {
+        animator.SetTrigger("Next");
+    }
+	
+}
