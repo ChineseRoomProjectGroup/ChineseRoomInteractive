237994569c65ed29a3b1fec2b3a22dbc9f595fdc
Robert Wild <wildr2@rpi.edu>
2015-04-30 19:38:50 -0400

    Add Hide Snap Objects Functionality
    snap objects can now be used as item placement indicators that show only
    when an item is held

--- ChineseRoomInteractiveUnity/Assets/Scripts/GrabItem.cs
+++ ChineseRoomInteractiveUnity/Assets/Scripts/GrabItem.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 
 public class GrabItem : MonoBehaviour 
 {
-    public List<Transform> snap_transforms;
+    // Objects whose positions are placement locations for grabbed items
+    // Can be used as placement indicators (snap objects will be active only when the GrabItem is grabbed)
+    public List<Transform> snap_objects; 
 
     private Vector2 target_pos;
     private bool dropping = false;
@@ -18,8 +20,11 @@ public class GrabItem : MonoBehaviour
 
     public void Start()
     {
+        // hide snap objects
+        ShowSnapObjects(false);
+
         // find/set whether dropable
-        if (snap_transforms.Count > 0)
+        if (snap_objects.Count > 0)
         {
             dropable = true;
         }
@@ -56,12 +61,15 @@ public class GrabItem : MonoBehaviour
     public void Grab()
     {
         grabbed = true;
+
+        // show snap objects
+        ShowSnapObjects(true);
     }
     /// <summary>
     /// Causes the object to position itself at the closest snap location
     /// 
-    /// requires: - if dropable, snap_transforms is not empty
-    ///           - snap_transforms contains non null elements
+    /// requires: - if dropable, snap_objects is not empty
+    ///           - snap_objects contains non null elements
     /// </summary>
     public void Drop()
     {
@@ -73,18 +81,37 @@ public class GrabItem : MonoBehaviour
 
         // find the closest snap position 
         float min_dist = float.MaxValue;
-        Transform closest_t = snap_transforms[0];
+        Transform closest_obj = snap_objects[0];
 
-        foreach (Transform snap_t in snap_transforms)
+        foreach (Transform snap_obj in snap_objects)
         {
-            float dist = Vector2.Distance(transform.position, snap_t.position);
+            float dist = Vector2.Distance(transform.position, snap_obj.position);
             if (dist < min_dist)
             {
                 min_dist = dist;
-                closest_t = snap_t;
+                closest_obj = snap_obj;
             }
         }
-        target_pos = closest_t.position;
+        target_pos = closest_obj.position;
+
+
+        // hide snap objects
+        ShowSnapObjects(false);
+    }
+
+
+    // PRIVATE MODIFIERS
+
+    /// <summary>
+    /// Show or hide snap objects (snap objects can be used as item placement indicators).
+    /// </summary>
+    /// <param name="show"></param>
+    private void ShowSnapObjects(bool show)
+    {
+        foreach (Transform snap_object in snap_objects)
+        {
+            snap_object.gameObject.SetActive(show);
+        }
     }
 
 
