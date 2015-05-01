1e9196c6ab2f0812898271b0a55c4bd60e49bf5d
SongYe <yes@rpi.edu>
2015-05-01 14:21:11 -0400

    Added pencil / symbol lists Room rearrange

---
+++ ChineseRoomInteractiveUnity/Assets/Scripts/Pencil.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pencil : GrabItem {
+
+    bool input_action = Input.GetMouseButtonDown(0);
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (input_action) // pencil drops
+        {
+       
+            if(true) // pencil drops on an answer slot on paper
+            {
+                // the symbol list pops up
+            }
+        }
+	}
+}
---
+++ ChineseRoomInteractiveUnity/Assets/Scripts/SymbolList.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SymbolList : MonoBehaviour {
+
+    private List<Transform> symbols;
+
+	void Start () {
+	    // scale list display box to fit the symbols
+        //x = symbols.size() * 20 + 20;
+	}
+	
+	void Update () {
+	
+	}
+}
